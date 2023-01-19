4ff72139260d45a19138af71b94dda49f37e1e2b
Dan Hancox <dthancox@gmail.com>
2023-01-19 10:14:04 +0000

    files moved into directories

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import App from './components/App.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,6 +10,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
 
 
