756e12306996cc75c2f6c0f0f4a914eaa88d86e6
Dan Hancox <dthancox@gmail.com>
2023-01-23 10:03:45 +0000

    LocationDetails test and App test passes

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,12 @@
-
 import React from "react";
 import PropTypes from "prop-types";
 import "../styles/App.css";
 import LocationDetails from "./LocationDetails";
 
-
 function App({ location }) {
-  const {city, country} = location;
-  return (
-    <LocationDetails
-      city={city}
-      country={country}
-    />
-  );
-};
+  const { city, country } = location;
+  return <LocationDetails city={city} country={country} />;
+}
 
 App.propTypes = {
   location: PropTypes.shape({
@@ -21,5 +14,5 @@ App.propTypes = {
     country: PropTypes.string,
   }).isRequired,
 };
-//<img src={} className="App-logo" alt="logo" /> belongs under header classname
+
 export default App;
