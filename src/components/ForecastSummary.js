099d79273e22eb9fbab63c72cd883fbfdc4d680a
Dan Hancox <dthancox@gmail.com>
2023-01-23 13:02:27 +0000

    Forecast Summary tests passed

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -7,7 +7,7 @@ function ForecastSummary(props) {
   return (
     <div className="forecast-summary">
       <div className="forecast-summary__date">{date}</div>
-      <div className="forecast-summary__icon" data-test="forecast-icon">{icon}</div>
+      <div className="forecast-summary__icon" data-testid="forecast-icon">{icon}</div>
       <div className="forecast-summary__temperature">
         {temperature.max}
         &deg;C
