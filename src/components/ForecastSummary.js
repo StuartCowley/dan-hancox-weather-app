7cb17ffbac0de74db8fcd3791996715c6324beba
Dan Hancox <dthancox@gmail.com>
2023-01-23 12:44:24 +0000

    Forecast Summary added and tested

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -2,26 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function ForecastSummary(props) {
-    const { date, description, icon, temperature } = props;
+  const { date, description, icon, temperature } = props;
 
-return (
+  return (
     <div className="forecast-summary">
-        <div className="forecast-summary__date">
-            {date}
-    </div>
-        <div className="forecast-summary__icon">
-        {icon}
-    </div>
-        <div className="forecast-summary__temperature">
+      <div className="forecast-summary__date">{date}</div>
+      <div className="forecast-summary__icon" data-test="forecast-icon">{icon}</div>
+      <div className="forecast-summary__temperature">
         {temperature.max}
         &deg;C
+      </div>
+      <div className="forecast-summary__description">{description}</div>
     </div>
-        <div className="forecast-summary__description">
-        {description}
-        </div>
-    </div>
-    );
-};
+  );
+}
 
 export default ForecastSummary;
 
--- src/tests/components/ForecastSummary.test.js
+++ src/tests/components/ForecastSummary.test.js
@@ -3,7 +3,8 @@ import { render } from "@testing-library/react";
 import ForecastSummary from "../../components/ForecastSummary";
 
 describe("ForecastSummary", () => {
-  const validProps = {date: 1111111,
+  const validProps = {
+    date: 1111111,
     description: "Stub description",
     icon: "stubIcon",
     temperature: {
@@ -11,18 +12,16 @@ describe("ForecastSummary", () => {
       max: 22,
     },
   };
-
-
-it("renders correctly", () => {
-  const { asFragment } = render(
-    <ForecastSummary
-      date={validProps.date}
-      description={validProps.description}
-      icon={validProps.icon}
-      temperature={validProps.temperature}
-    />
-  );
+  it("renders correctly", () => {
+    const { asFragment } = render(
+      <ForecastSummary
+        date={validProps.date}
+        description={validProps.description}
+        icon={validProps.icon}
+        temperature={validProps.temperature}
+      />
+    );
 
   expect(asFragment()).toMatchSnapshot();
-});
+  });
 });
