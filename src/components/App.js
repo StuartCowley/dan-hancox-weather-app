e1789831bc8363bc2b528b72dd22b35d35a585f2
Dan Hancox <dthancox@gmail.com>
2023-01-23 17:08:43 +0000

    added ForecastSummaries component

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,28 @@ import "../styles/App.css";
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
 
-function App(props) {
-  const { location } = props;
+function App({ location, forecasts }) {
   return (
-    <div className="App">
+    <div className="forecast">
       <LocationDetails city={location.city} country={location.country} />
-      <ForecastSummaries />
+
+      <ForecastSummaries forecasts={forecasts} />
     </div>
   );
 }
 
 App.propTypes = {
+  forecasts: PropTypes.arrayOf(
+    PropTypes.shape({
+      date: PropTypes.number,
+      description: PropTypes.string,
+      icon: PropTypes.string,
+      temperature: PropTypes.shape({
+        max: PropTypes.number,
+        min: PropTypes.number,
+      }),
+    })
+  ).isRequired,
   location: PropTypes.shape({
     city: PropTypes.string,
     country: PropTypes.string,
