811d28fda897c4340b6f2a56167f2e212641e90b
Dan Hancox <dthancox@gmail.com>
2023-01-31 11:40:50 +0000

    Search Form Button added

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import getForecast from "../requests/getForecast";
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
 import ForecastDetails from "./ForecastDetails";
+import SearchForm from "./SearchForm";
 
 function App() {
   const [selectedDate, setSelectedDate] = useState(0);
@@ -26,6 +27,8 @@ function App() {
   return (
     <div className="weather-app">
       <LocationDetails city={location.city} country={location.country} />
+
+      <SearchForm />
 
       <ForecastSummaries
         forecasts={forecasts}
