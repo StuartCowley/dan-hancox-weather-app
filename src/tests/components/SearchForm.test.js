100d4376168545ce656c83398a1e082cd58192c7
Dan Hancox <dthancox@gmail.com>
2023-02-01 11:42:10 +0000

    CSS added

--- src/tests/components/SearchForm.test.js
+++ src/tests/components/SearchForm.test.js
@@ -3,7 +3,6 @@ import { render } from "@testing-library/react";
 import SearchForm from "../../components/SearchForm";
 
 describe("it renders correctly", () => {
-
   const validProps = {
     searchText: "Bristol",
     setSearchText: () => {},
@@ -11,13 +10,14 @@ describe("it renders correctly", () => {
   };
 
   it("renders a search box and button", () => {
-    const { asFragment } = render(<SearchForm
+    const { asFragment } = render(
+      <SearchForm
         searchText={validProps.searchText}
         setSearchText={validProps.setSearchText}
         onSubmit={validProps.handleCitySearch}
-        />);
-    
-    expect(asFragment()).toMatchSnapshot();
+      />
+    );
 
+    expect(asFragment()).toMatchSnapshot();
   });
 });
