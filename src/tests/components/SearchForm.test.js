4c1e41be7cea0997404e2a5b2edf19531d51f383
Dan Hancox <dthancox@gmail.com>
2023-02-01 10:39:06 +0000

    Test for SearchForm Passing

--- src/tests/components/SearchForm.test.js
+++ src/tests/components/SearchForm.test.js
@@ -3,8 +3,21 @@ import { render } from "@testing-library/react";
 import SearchForm from "../../components/SearchForm";
 
 describe("it renders correctly", () => {
+
+  const validProps = {
+    searchText: "Bristol",
+    setSearchText: () => {},
+    onSubmit: () => {},
+  };
+
   it("renders a search box and button", () => {
-    const { asFragment } = render(<SearchForm searchText setSearchText onSubmit/>);
+    const { asFragment } = render(<SearchForm
+        searchText={validProps.searchText}
+        setSearchText={validProps.setSearchText}
+        onSubmit={validProps.handleCitySearch}
+        />);
+    
     expect(asFragment()).toMatchSnapshot();
+
   });
 });
